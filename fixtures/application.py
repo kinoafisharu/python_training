df3f4fcbdaf20d901b318afbdab0e078d48a9b90
Mikhail Ivanov <kinoafisharu@gmail.com>
2018-05-30 15:17:12 +0300

    выделен класс обджектхэлпер для работы с объектами группа и контакт

--- fixtures/application.py
+++ fixtures/application.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixtures.session import SessionHelper
+from fixtures.object import ObjectHelper
 
 class Application:
 
@@ -8,54 +9,7 @@ class Application:
         self.wd = WebDriver(capabilities={"marionette": False}, firefox_binary="/home/asteroid/programms/firefox/firefox")
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
-
-
-    def create_person_form(self, person):
-        wd = self.wd
-        self.open_home_page()
-        # init_person_creation
-        wd.find_element_by_link_text("add new").click()
-        # fill_person_form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(person.name)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(person.lastname)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(person.address)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(person.mobile)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(person.email)
-        # submit_person_creation
-        wd.find_element_by_name("submit").click()
-
-    def return_to_group_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group_form(self, group):
-        wd = self.wd
-        self.open_home_page()
-        # init_group_creation
-        wd.find_element_by_name("new").click()
-        # fill_group_form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit_group_creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_group_page()
+        self.object = ObjectHelper(self)
 
     def open_home_page(self):
         wd = self.wd
--- tests/test_for_addressbook.py
+++ tests/test_for_addressbook.py
@@ -13,17 +13,17 @@ def app(request):
 
 def test_add_group(app):
         app.session.login(username="admin", password="secret")
-        app.create_group_form(Group(name="test progon", header="jhvgvhgv", footer="khgcvkvv"))
+        app.object.create_group_form(Group(name="test progon", header="jhvgvhgv", footer="khgcvkvv"))
         app.session.logout()
 
 def test_add_empty_group(app):
         app.session.login(username="admin", password="secret")
-        app.create_group_form(Group(name="", header="", footer=""))
+        app.object.create_group_form(Group(name="", header="", footer=""))
         app.session.logout()
 
 def test_add_person(app):
         app.session.login(username="admin", password="secret")
-        app.create_person_form(Person(name="1", lastname="2", address="3", mobile="4", email="5"))
+        app.object.create_person_form(Person(name="1", lastname="2", address="3", mobile="4", email="5"))
         app.session.logout()
 
 
